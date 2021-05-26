b2b85b19430499e075d0af45c4f95171cf3bddef
Chris Boydstun <52934303+cboydstun@users.noreply.github.com>
2021-05-26 12:27:01 -0500

    added field validation

--- src/App.js
+++ src/App.js
@@ -1,20 +1,60 @@
-import React from 'react'
+import React, {useState} from 'react'
 
 import './App.css'
 
 export default function App() {
+    const [values, setValues] = useState({
+        firstName: '',
+        lastName: '',
+        email: '',
+    });
+
+    const [submitted, setSubmitted] = useState(false);
+
+    const handleFirstNameInputChange = (event) => {
+        event.persist();
+        setValues((values) => ({
+            ...values,
+            firstName: event.target.value,
+        }));
+    };
+
+    const handleLastNameInputChange = (event) => {
+        event.persist();
+        setValues((values) => ({
+            ...values,
+            lastName: event.target.value,
+        }));
+    };
+
+    const handleEmailInputChange = (event) => {
+        event.persist();
+        setValues((values) => ({
+            ...values,
+            email: event.target.value,
+        }));
+    };
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        setSubmitted(true);
+    }
+
     return (
     <div class="form-container">
-      <form class="register-form">
+      <form class="register-form" onSubmit={handleSubmit}>
         {/* Uncomment the next line to show the success message */}
-        {/* <div class="success-message">Success! Thank you for registering</div> */}
         <input
           id="first-name"
           class="form-field"
           type="text"
           placeholder="First Name"
           name="firstName"
+          value={values.firstName}
+          onChange={handleFirstNameInputChange}
         />
+        {submitted && !values.firstName && <span id='first-name-error'>Please enter a first name</span>}
+
         {/* Uncomment the next line to show the error message */}
         {/* <span id="first-name-error">Please enter a first name</span> */}
         <input
@@ -23,22 +63,30 @@ export default function App() {
           type="text"
           placeholder="Last Name"
           name="lastName"
+          value={values.lastName}
+          onChange={handleLastNameInputChange}
         />
-        {/* Uncomment the next line to show the error message */}
-        {/* <span id="last-name-error">Please enter a last name</span> */}
+        {submitted && !values.lastName && <span id='last-name-error'>Please enter a last name</span>}
+
+
         <input
           id="email"
           class="form-field"
           type="text"
           placeholder="Email"
           name="email"
+          value={values.email}
+          onChange={handleEmailInputChange}
         />
-        {/* Uncomment the next line to show the error message */}
-        {/* <span id="email-error">Please enter an email address</span> */}
+        {submitted && !values.email && <span id='email-error'>Please enter an email address</span>}
+
         <button class="form-field" type="submit">
           Register
         </button>
       </form>
+    
+    {submitted && <div class='success-message'>Success! Thank you for registering</div>}
+
     </div>
     )
 }
